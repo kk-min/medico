12fc60ffc950b1b720031854309776d002b45834
MINK <76023265+mink0003@users.noreply.github.com>
2022-08-24 15:15:08 +0800

    Initialize Firebase

---
+++ src/firebase.js
@@ -0,0 +1,39 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+	apiKey: "",
+
+	authDomain: "",
+
+	projectId: "",
+
+	storageBucket: ".appspot.com",
+
+	messagingSenderId: "",
+
+	appId: "1::web:a36e8494f2f8ecf6dc3fe6",
+};
+
+// Initialize Firebase
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth();
+setPersistence(auth, browserSessionPersistence)
+	.then(() => {})
+	.catch((error) => {
+		// Handle Errors here.
+		const errorCode = error.code;
+		const errorMessage = error.message;
+	});
+
+export { db, auth };
